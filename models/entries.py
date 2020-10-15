4f03ce618246203ef6101adc5885940389ebd2d2
Joshua Medlen <joshua246@gmail.com>
2020-10-15 15:38:45 -0500

    setup SQL and setup server

---
+++ models/entries.py
@@ -0,0 +1,8 @@
+class Entries():
+
+    def __init__(self, id, concept, entry, date, moodId):
+        self.id = id
+        self.concept = concept
+        self.entry = entry
+        self.date = date
+        self.moodId = moodId
